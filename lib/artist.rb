da38debdfe27959b1691a6d06e5a9723910d3fac
Sandy Zhang <sandyzhang142@gmail.com>
2022-12-15 13:55:34 -0500

    commit tree 698aa44187c3cccf2ff3a33ca71f56f9cd6f292c

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,6 +4,9 @@ class Artist
   attr_accessor :name
   attr_reader :songs
 
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+
   @@artists = []
 
   def initialize
@@ -19,13 +22,13 @@ class Artist
     @@artists
   end
 
-  def self.reset_all
-    self.all.clear
-  end
+  # def self.reset_all
+  #   self.all.clear
+  # end
 
-  def self.count
-    self.all.count
-  end
+  # def self.count
+  #   self.all.count
+  # end
 
   def add_song(song)
     @songs << song
